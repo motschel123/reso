1a68ad95cc4890047f21c89ba56c1523cc3a0e21
Luca Beetz <luca-beetz@tutanota.com>
2021-06-01 18:02:27 +0200

    Add Button Text color to Theme

--- lib/consts/theme.dart
+++ lib/consts/theme.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   textTheme: TextTheme(
-      headline1: const TextStyle(fontSize: 24.0, color: Colors.black),
-      headline2: const TextStyle(fontSize: 18.0, color: Colors.black),
-      headline3: const TextStyle(fontSize: 14.0, color: Colors.black),
-      subtitle1:
-          TextStyle(fontSize: 14.0, color: Colors.black.withOpacity(0.5)),
-      bodyText1: const TextStyle(fontSize: 10.0, color: Colors.black),
-      bodyText2:
-          TextStyle(fontSize: 10.0, color: Colors.black.withOpacity(0.5))),
+    headline1: const TextStyle(fontSize: 24.0, color: Colors.black),
+    headline2: const TextStyle(fontSize: 18.0, color: Colors.black),
+    headline3: const TextStyle(fontSize: 14.0, color: Colors.black),
+    subtitle1: TextStyle(fontSize: 14.0, color: Colors.black.withOpacity(0.5)),
+    bodyText1: const TextStyle(fontSize: 12.0, color: Colors.black),
+    bodyText2: TextStyle(fontSize: 12.0, color: Colors.black.withOpacity(0.5)),
+    button: const TextStyle(fontSize: 18.0, color: Colors.white),
+  ),
 );
