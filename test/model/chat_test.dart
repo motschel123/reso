b5b6d21e2b352ce9c8e87ad5124b48395a6a5d8d
Luca Beetz <luca-beetz@tutanota.com>
2021-06-11 11:52:24 +0200

    Add test for Chat from chatDoc

--- test/model/chat_test.dart
+++ test/model/chat_test.dart
@@ -9,7 +9,8 @@ void main() {
   group('Chat model', () {
     test('should create Map from Chat', () async {
       final FakeFirebaseFirestore firestore = FakeFirebaseFirestore();
-      await firestore.collection(OFFERS_COLLECTION).add(<String, dynamic>{
+      final DocumentReference<Map<String, dynamic>> relatedOffer =
+          await firestore.collection(OFFERS_COLLECTION).add(<String, dynamic>{
         OFFER_TYPE: OFFER_TYPE_FOOD,
         OFFER_TITLE: 'Outdoor walk',
         OFFER_DESCRIPTION: 'Short group walk',
@@ -18,10 +19,6 @@ void main() {
         OFFER_AUTHOR_DISPLAY_NAME: 'Luca',
         OFFER_AUTHOR_IMAGE_URL: 'https://thispersondoesnotexist.com/image',
       });
-      final QuerySnapshot<Map<String, dynamic>> snapshot =
-          await firestore.collection(OFFERS_COLLECTION).get();
-      final DocumentReference<Map<String, dynamic>> relatedOffer =
-          snapshot.docs.first.reference;
 
       final Chat chat = Chat(
           chatId: '1ad',
@@ -38,6 +35,40 @@ void main() {
         CHAT_DATE_CREATED: DateTime(2021, 6, 11, 10, 44),
         CHAT_PEER_DATA: <String, dynamic>{},
       });
+    });
+
+    test('should create Chat from chatDoc', () async {
+      final FakeFirebaseFirestore firestore = FakeFirebaseFirestore();
+      // Add sample offer to firestore
+      final DocumentReference<Map<String, dynamic>> relatedOffer =
+          await firestore.collection(OFFERS_COLLECTION).add(<String, dynamic>{
+        OFFER_TYPE: OFFER_TYPE_FOOD,
+        OFFER_TITLE: 'Outdoor walk',
+        OFFER_DESCRIPTION: 'Short group walk',
+        OFFER_PRICE: 'free',
+        OFFER_AUTHOR_UID: '1a3',
+        OFFER_AUTHOR_DISPLAY_NAME: 'Luca',
+        OFFER_AUTHOR_IMAGE_URL: 'https://thispersondoesnotexist.com/image',
+      });
+
+      // Add sample chat to firestore
+      await firestore.collection(CHATS_COLLECTION).add(<String, dynamic>{
+        CHAT_LATEST_DATE: DateTime(2021, 6, 11, 11, 4),
+        CHAT_LATEST_MESSAGE_TEXT: 'Hey',
+        CHAT_DATE_CREATED: DateTime(2021, 6, 11, 11, 4),
+        CHAT_RELATED_OFFER: relatedOffer,
+        CHAT_PEER_DATA: <String, dynamic>{},
+      });
+
+      final QuerySnapshot<Map<String, dynamic>> chatSnapshot =
+          await firestore.collection(CHATS_COLLECTION).get();
+      final Chat chat = Chat.fromChatDoc(chatSnapshot.docs.first);
+
+      expect(chat.chatId, chatSnapshot.docs.first.id);
+      expect(chat.dateCreated, DateTime(2021, 6, 11, 11, 4));
+      expect(chat.latestDate, DateTime(2021, 6, 11, 11, 4));
+      expect(chat.latestMessageText, 'Hey');
+      expect(chat.relatedOffer, relatedOffer);
     });
   });
 }
