250a78087e190205895ee91aea9d571123ee2877
Motschel <marcel.schoeckel@gmail.com>
2021-06-28 21:32:53 +0200

    implemented chat manager into messaging screen

--- lib/ui/screens/main/messaging.dart
+++ lib/ui/screens/main/messaging.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reso/business_logic/providers/chat_manager.dart';
+import 'package:reso/business_logic/services/chat_service.dart';
+import 'package:reso/business_logic/services/offer_service.dart';
+import 'package:reso/consts/firestore.dart';
+import 'package:reso/model/offer.dart';
 
 class Messaging extends StatefulWidget {
   const Messaging({Key? key}) : super(key: key);
@@ -12,14 +19,43 @@ class _MessagingState extends State<Messaging> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
+            child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Consumer<ChatManager>(
+        builder: (context, value, child) => Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Text('Nachrichten', style: Theme.of(context).textTheme.headline1),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: value.chats.length,
+                  itemBuilder: (context, index) => TextButton(
+                        onPressed: () async {
+                          DocumentSnapshot<Map<String, dynamic>> offerDoc =
+                              await FirebaseFirestore.instance
+                                  .doc(OFFERS_COLLECTION +
+                                      '/' +
+                                      value.chats[index].offerId)
+                                  .get();
+                          if (offerDoc.data() == null) {
+                            throw Exception(
+                                "offerDoc (${offerDoc.id}) has no data");
+                          } else {
+                            Offer offer = Offer.fromMap(offerDoc.data()!);
+
+                            ChatService.openChat(
+                                context: context,
+                                chat: value.chats[index],
+                                offer: offer);
+                          }
+                        },
+                        child: Text(value.chats[index].chatId),
+                      )),
+            ),
           ],
         ),
       ),
-    ));
+    )));
   }
 }
