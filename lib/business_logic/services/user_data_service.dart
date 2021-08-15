179830b137fa4d4de370b349cea9bf9ecf58992d
Motschel <marcel.schoeckel@gmail.com>
2021-08-15 15:17:30 +0200

    added updateUserData

--- lib/business_logic/services/user_data_service.dart
+++ lib/business_logic/services/user_data_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:reso/business_logic/services/storage_service.dart';
 import 'package:reso/consts/firestore.dart';
 import 'package:reso/model/user_profile.dart';
 
@@ -18,12 +19,43 @@ class UserDataService {
       );
     }
 
-    return _firestore
-        .collection(USERS_COLLECTION)
-        .doc(uid)
-        .get()
-        .then((DocumentSnapshot<Map<String, dynamic>> docSnap) {
-      return UserProfile.fromDoc(docSnap);
-    });
+    return _firestore.collection(USERS_COLLECTION).doc(uid).get().then(
+        (DocumentSnapshot<Map<String, dynamic>> docSnap) =>
+            UserProfile.fromDoc(docSnap));
+  }
+
+  static Future<void> updateUserData(
+      String? newDisplayName, String? newImageRef) async {
+    // check user is signed in
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) {
+      throw Exception('No user signed in to update data on');
+    }
+
+    // data map for firestore update
+    final Map<String, dynamic> updates = <String, dynamic>{};
+    // future array to wait for all tasks to complete
+    final List<Future<void>> futures = <Future<dynamic>>[];
+
+    if (newDisplayName != null) {
+      updates.addAll(<String, dynamic>{USER_DISPLAY_NAME: newDisplayName});
+      futures.add(currentUser.updateDisplayName(newDisplayName));
+    }
+
+    if (newImageRef != null) {
+      updates.addAll(<String, dynamic>{USER_IMAGE_REFERENCE: newImageRef});
+      futures.add(StorageService.getImageUrl(newImageRef).then<Future<void>>(
+        (String photoUrl) => currentUser.updatePhotoURL(photoUrl),
+      ));
+    }
+
+    futures.add(
+      FirebaseFirestore.instance
+          .collection(USERS_COLLECTION)
+          .doc(currentUser.uid)
+          .update(updates),
+    );
+
+    return Future.wait(futures).then((_) => null);
   }
 }
